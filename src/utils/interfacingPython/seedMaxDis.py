5257ade9e86ecf8fa4f2de533a983083ed2c5255
Windowqbn <Windowqbn@nothing.com>
2024-12-08 22:10:25 +1100

    Add REFERENCE for seedMaxDis()

--- src/utils/interfacingPython/seedMaxDis.py
+++ src/utils/interfacingPython/seedMaxDis.py
@@ -18,5 +18,7 @@ def seedMaxDis(Area, numSeed):
     if numSeed<=0:
         raise ValueError("numSeed must be larger than 0")
     
-    rMax=math.sqrt(2*Area/numSeed*math.sqrt(3))
+    """ Refer to Appendix A - Designing for Disorder: The Mechanical Behaviour of 
+    Bioinspired, Stochastic Honeycomb Materials  by Derek Alexandre Aranguren van Egmond (University of Toronto, 2018) """
+    rMax=math.sqrt(2*Area/numSeed*math.sqrt(3)) 
     return rMax
